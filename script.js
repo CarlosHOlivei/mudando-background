23ba61f1970a1c541bc274d959535ab34452e8f3
Carlos <olv.carlos@hotmail.com>
2023-03-15 14:13:50 -0300

    Corrigi o posicionamento da Function corRGB

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function corRGB(){
-    var r = Math.floor(Math.random()*(255+1))
-    var g = Math.floor(Math.random()*(255+1))
-    var b = Math.floor(Math.random()*(255+1))
-    return `rgb(${r}, ${g}, ${b})`;
-} 
 var corAleat = corRGB();
 const cores = ["red", "green", corAleat, "#f15025"];
 const botao = document.getElementById("botao");
@@ -14,3 +8,9 @@ botao.addEventListener('click', function(){
     document.body.style.backgroundColor = cores[numAleat];
     cor.textContent = cores[numAleat];
 })
+function corRGB(){
+    var r = Math.floor(Math.random()*(255+1))
+    var g = Math.floor(Math.random()*(255+1))
+    var b = Math.floor(Math.random()*(255+1))
+    return `rgb(${r}, ${g}, ${b})`;
+} 
